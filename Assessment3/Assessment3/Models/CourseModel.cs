6d6202ae9604251c20fa4a820a73436f86fb34eb
Sushil Shakya <90746527+sushilsayshello@users.noreply.github.com>
2022-09-17 13:11:20 +1000

    Update CourseModel.cs

--- Assessment3/Assessment3/Models/CourseModel.cs
+++ Assessment3/Assessment3/Models/CourseModel.cs
@@ -9,7 +9,11 @@ namespace Assessment3.Models
     public class CourseModel
     {
         
-
+        [Key]
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public string CourseUnits { get; set; }
+        public string CourseDescription { get; set; }
        
 
     }
