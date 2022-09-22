4ab0a0242fb8942104acec72dcfac699668cb4a8
Sushil Shakya <90746527+sushilsayshello@users.noreply.github.com>
2022-09-22 16:35:49 +1000

    Update StudentModel.cs

--- Assessment3/Assessment3/Models/StudentModel.cs
+++ Assessment3/Assessment3/Models/StudentModel.cs
@@ -11,36 +11,31 @@ namespace Assessment3.Models
     {
         [Key]
 	[Required(ErrorMessage = "StudentID is required")]
+	[StringLength(7, ErrorMessage = "Max 7 digits")]
         public int StudentId { get; set; }
+	    
         [Required(ErrorMessage = "FirstName is required")]
+	[StringLength(16, MinimumLength = 4, ErrorMessage = "at least 4 characters but no more than 16 characters.")]
         public string FirstName { get; set; }
+	    
+	[Required(ErrorMessage = "LastName is required")]
+        [StringLength(16, MinimumLength = 4, ErrorMessage = " at least 4 characters but no more than 16 characters.")]   
         public string LastName { get; set; }
 
-        [Required(ErrorMessage = "Email is required")]
+      	[Required(ErrorMessage = "Email is required")]
+        [RegularExpression("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}+$", ErrorMessage = "Email is required with matching regular expression"]
         public string Email { get; set; }
 
         [DisplayName("Address")]
         [StringLength(1024)]
         public string Address { get; set; }
+			   
+	[Required(ErrorMessage = "Contact is required")
+	[StringLength(10, ErrorMessage = "Max 10 digits")]
         public string Contact { get; set; }
-        
-        
-		
-		public int StudentID { get; set; }
 
-		[Required(ErrorMessage = "LastName is required")]
-		[StringLength(16, MinimumLength = 4 ,ErrorMessage = "Must be atleast 4 characters but no more than 16 characters")]
-		public string LastName { get; set; }
 
-		[Required(ErrorMessage = "FirstName is required")]
-		[StringLength(16, MinimumLength = 4, ErrorMessage = "Must be atleast 4 characters but no more than 16 characters")]
-		public string FirstName { get; set; }
-
-		public string Phone { get; set; }
-
-		[Required(ErrorMessage = "Email is required")]
-		[RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
-		public string Email { get; set; 
+        [
 
         //public ICollection<StudentResourceModel> StudentResourceModels { get; set; }
     }
