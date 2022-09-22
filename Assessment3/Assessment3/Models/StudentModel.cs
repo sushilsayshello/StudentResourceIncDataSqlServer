707176651118e939f7fcbe5cf6dc9a94a27f3477
Sushil Shakya <90746527+sushilsayshello@users.noreply.github.com>
2022-09-22 16:32:26 +1000

    Update StudentModel.cs

--- Assessment3/Assessment3/Models/StudentModel.cs
+++ Assessment3/Assessment3/Models/StudentModel.cs
@@ -22,6 +22,24 @@ namespace Assessment3.Models
         [StringLength(1024)]
         public string Address { get; set; }
         public string Contact { get; set; }
+        
+        [Required(ErrorMessage = "StudentID is required")]
+		[MinLength(7,ErrorMessage = "Must be atleast 7")]
+		public int StudentID { get; set; }
+
+		[Required(ErrorMessage = "LastName is required")]
+		[StringLength(16, MinimumLength = 4 ,ErrorMessage = "Must be atleast 4 characters but no more than 16 characters")]
+		public string LastName { get; set; }
+
+		[Required(ErrorMessage = "FirstName is required")]
+		[StringLength(16, MinimumLength = 4, ErrorMessage = "Must be atleast 4 characters but no more than 16 characters")]
+		public string FirstName { get; set; }
+
+		public string Phone { get; set; }
+
+		[Required(ErrorMessage = "Email is required")]
+		[RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
+		public string Email { get; set; 
 
         //public ICollection<StudentResourceModel> StudentResourceModels { get; set; }
     }
